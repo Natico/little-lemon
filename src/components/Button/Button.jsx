0661bdda3ce5d1030210f4f5a0b9fb15297aef38
n.amjadian <amjadian.n@gmail.com>
2026-08-31 17:06:47 +0330

    feat: add Little Lemon design system

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import "./Button.css";
+
+function Button({ children, type = "button", onClick, disabled = false }) {
+  return (
+    <button
+      className="button"
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
